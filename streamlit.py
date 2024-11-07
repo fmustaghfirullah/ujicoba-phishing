dd3778e5a831ae7fef3d6beddf6defc8fc540f40
fmustaghfirullah <52424945+fmustaghfirullah@users.noreply.github.com>
2024-11-07 17:11:51 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score, f1_score
+from sklearn.impute import SimpleImputer
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 import joblib
@@ -36,6 +37,9 @@ X_transformed = vectorizer.fit_transform(X)
 
 # Split data menjadi training dan testing set
 X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state=42)
+imputer = SimpleImputer(strategy='mean')  
+X_train = imputer.fit_transform(X_train)
+X_test = imputer.transform(X_test)
 
 # Train model Random Forest
 model = RandomForestClassifier(n_estimators=100, random_state=42)
